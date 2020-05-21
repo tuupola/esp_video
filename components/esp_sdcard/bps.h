fb0bd3efc29d5a3b4be6cb39b8f82fb7ff4db4fa
Mika Tuupola <tuupola@appelsiini.net>
2020-05-21 14:11:03 +0700

    Refactor to inlined bps counter

--- components/esp_sdcard/bps.h
+++ components/esp_sdcard/bps.h
@@ -38,8 +38,49 @@ extern "C" {
 #include <stdint.h>
 #include <stddef.h>
 
-float bps(size_t incoming);
-void bps_reset();
+#define BPS_RESET   (SIZE_MAX)
+
+static inline float bps(size_t add)
+{
+    static struct timespec start;
+    static uint64_t value = 0;
+    static float current = 0.0;
+    static bool firstrun = true;
+
+    struct timespec now;
+    static uint32_t seconds;
+    float measured = 0.0;
+
+    /* Larger value is less smoothing. */
+    float smoothing = 0.95;
+
+    if (firstrun) {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        firstrun = false;
+    }
+
+    if (BPS_RESET == add) {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        value = 0;
+        current = 0;
+        firstrun = false;
+        return 0;
+    }
+
+    value += add;
+
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    seconds = now.tv_sec - start.tv_sec;
+
+    if (seconds) {
+        measured = value / (float) seconds;
+        current = (measured * smoothing) + (current * (1.0 - smoothing));
+
+        return current;
+    }
+
+    return 0;
+}
 
 #ifdef __cplusplus
 }
